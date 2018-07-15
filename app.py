57c3cee00c58d40a5db1ebb4054fed84f2a9562d
charlieanna <ankothari@gmail.com>
2018-07-14 22:05:05 -0400

    First iteration complete'

--- app.py
+++ app.py
@@ -27,9 +27,35 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from flask_api import FlaskAPI
 from flask_caching import Cache
+import json
+# from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# from datetime import datetime
+# db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/charlieana"
 
 
-app = FlaskAPI(__name__)
+# class User(db.Model):
+#   __tablename__ = 'users'
+
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String(80), unique=True, nullable=False)
+#   created_at = db.Column(db.DateTime,default=datetime.utcnow)
+#   updated_at = db.Column(db.DateTime,default=datetime.utcnow)
+  
+#   def __repr__(self):
+#       return '<User %r>' % self.name
+
+# class Tag(db.Model):
+#   __tablename__ = 'tags'
+
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String(80), unique=True, nullable=False)
+  
+#   def __repr__(self):
+#       return '<Tag %r>' % self.name
+
 
 cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
@@ -45,6 +71,7 @@ mail = Mail(app)
 
 @app.route("/")
 def index():
+    
     # defining the api-endpoint
     # API_ENDPOINT = "https://stackoverflow.com/oauth/access_token"
     # # data to be sent to api
@@ -60,34 +87,34 @@ def index():
     # response = r.text
     # access_token = response.split("=")[1]
     # print("access_token:", response.split("=")[1])
+    # print(Tag.query.all())
 
+    # jobstores = {
+    #     'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+    # }
+    # executors = {
+    #     'default': ThreadPoolExecutor(20),
+    #     'processpool': ProcessPoolExecutor(5)
+    # }
+    # job_defaults = {
+    #     'coalesce': False,
+    #     'max_instances': 3
+    # }
+    # scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
 
-    jobstores = {
-        'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
-    }
-    executors = {
-        'default': ThreadPoolExecutor(20),
-        'processpool': ProcessPoolExecutor(5)
-    }
-    job_defaults = {
-        'coalesce': False,
-        'max_instances': 3
-    }
-    scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
+    # # scheduler.add_job(
+    # # func=send_questions,
+    # # trigger=IntervalTrigger(days = 1),
+    # # replace_existing=True)
+    # from datetime import date
+    # from datetime import datetime
+    # from datetime import timedelta
+    # scheduler.add_job(send_questions, 'date', run_date=datetime.now() + timedelta(seconds=2)  ,replace_existing=True)
 
-    # scheduler.add_job(
-    # func=send_questions,
-    # trigger=IntervalTrigger(days = 1),
-    # replace_existing=True)
-    from datetime import date
-    from datetime import datetime
-    from datetime import timedelta
-    scheduler.add_job(send_questions, 'date', run_date=datetime.now() + timedelta(seconds=2)  ,replace_existing=True)
-
-    # scheduler.add_job(print_date_time, 'date', run_date=date(2009, 11, 6), args=['text'])
-    # scheduler.start()
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
+    # # scheduler.add_job(print_date_time, 'date', run_date=date(2009, 11, 6), args=['text'])
+    # # scheduler.start()
+    # # Shut down the scheduler when exiting the app
+    # atexit.register(lambda: scheduler.shutdown())
 
     return render_template('index.html')
 
@@ -97,8 +124,8 @@ def search_tag():
   result = request.args
   search_tag = result.get("search_tag")
   fasttext_model = load_models()
-  similar_words = fasttext_model.wv.most_similar([search_tag], topn=20)
-  return dict(similar_words)
+  similar_words = fasttext_model.wv.most_similar([search_tag], topn=50)
+  return json.dumps(dict(similar_words))
 
 @app.route("/top")
 def top_questions():
@@ -109,7 +136,7 @@ def top_questions():
     data = json.load(f)
 
   response = {'data': data}
-  return response
+  return json.dumps(response)
 
 @app.route("/question")
 def question():
@@ -137,7 +164,7 @@ def question():
   job_config.query_parameters = query_params
   query_job = client.query(query, job_config=job_config)
   results = query_job.result()
-  return {'results': [{'question_id':row.question_id, 'answer_id': row.answer_id,'ascore': row.ascore, 'question': row.question} for row in results]}
+  return json.dumps({'results': [{'question_id':row.question_id, 'answer_id': row.answer_id,'ascore': row.ascore, 'question': row.question} for row in results]})
 
 def send_questions():
   with app.app_context():
@@ -194,6 +221,29 @@ def similar():
   results = query_job.result()
   return {'results': [{'id': row.id,'ascore': row.ascore, 'question': row.question} for row in results]}
 
+
+@app.route('/search_by_id',methods = ['POST', 'GET'])
+def search_by_id():
+  result = request.args
+  print(result)
+  question_id = result.get("id")
+  print(question_id)
+  query = """
+          SELECT id, title
+          FROM `bigquery-public-data.stackoverflow.posts_questions`
+          where  id = @a
+          """
+  query_params = [
+      bigquery.ScalarQueryParameter(
+          'a', 'INT64', question_id)
+      ]
+  job_config = bigquery.QueryJobConfig()
+  job_config.query_parameters = query_params
+  client = bigquery.Client.from_service_account_json('../../My_Project-c23185ac100b.json')
+  query_job = client.query(query, job_config=job_config)
+  results = query_job.result()
+  return json.dumps({'results': [{'id': row.id, 'question': row.title} for row in results]})
+
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
   all_questions = []
@@ -204,14 +254,14 @@ def result():
     tag_count_hash = None
     with open('../tag_count_score_hash.json') as f:
       tag_count_hash = json.load(f)
-      word2vec_model = load_models()
-      tags_similar = word2vec_model.wv.most_similar(result['word'], topn=30)
+      fasttext_model = load_models()
+      tags_similar = fasttext_model.wv.most_similar(result['word'], topn=30)
       related_tags = []
       for tag, similarity in tags_similar:
           related_tags.append({tag:tag_count_hash[tag]})
-
+    print(related_tags)
     response = {'data': tag_count_hash[result['word']], 'related_tags': related_tags}
-    return response
+    return json.dumps(response)
     # result = request.form
     # word2vec_model, fasttext_model = load_models()
     # # Once the model has been calculated, it is easy and fast to find most similar words.
